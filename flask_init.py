f17ed66ed922806df18d064c77f3b707518cb524
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-13 19:13:02 -0600

    Add procfile

--- flask_init.py
+++ flask_init.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/hello')
@@ -14,4 +16,5 @@ def user(username):
     return 'User %s' % username
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
